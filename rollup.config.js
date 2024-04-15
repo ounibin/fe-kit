935ad43949308553b2aca8e009e4cba5c988f212
wangkj <>
2024-04-15 19:18:20 +0800

    使用rollup进行打包ts

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    format: 'es',
+    file: 'dist/index.mjs'
+  },
+  plugins: [
+    commonjs(),
+    typescript()
+  ],
+  external: [/node_modules/],
+}
