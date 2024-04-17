366d1e6b0fe3bdd8d71ab6b5100657a70c759cc1
wangkj <>
2024-04-17 19:09:15 +0800

    feat: 添加numberKit

--- src/numberKit/index.ts
+++ src/numberKit/index.ts
@@ -3,7 +3,7 @@ enum FormatType {
   PERCENT = 'percent'
 }
 
-function format(num: number, formatType: FormatType = FormatType.DECIMAL, decimal = 2) {
+function format(num: number, formatType: FormatType = FormatType.DECIMAL, decimal = 2): string {
   return num.toLocaleString(undefined, {
     style: formatType,
     useGrouping: true,
@@ -12,6 +12,33 @@ function format(num: number, formatType: FormatType = FormatType.DECIMAL, decima
   })
 }
 
+function formatPrice(num: number, decimal = 2): string {
+  return format(num, FormatType.DECIMAL, decimal)
+}
+
+function formatPercent(num: number, decimal = 2): string {
+  return format(num, FormatType.DECIMAL, decimal)
+}
+
+
+enum LangType {
+  CN = 'cn',
+  EN = 'en',
+}
+function formatUnit(num: number, lang: LangType = LangType.CN): string {
+  const _num = Math.abs(num)
+  if (_num >= 10000) {
+    let unit = lang === 'cn' ? '万' : 'w'
+    const _n = _num / 10000
+    const formattedNum = (String(_n).indexOf('.') !== -1) ? _n.toFixed(1) : _n
+    return `${num < 0 ? '-' : ''}${formattedNum}${unit}`
+  } else {
+    return num.toString()
+  }
+}
+
 export default {
-  format
+  formatPrice,
+  formatPercent,
+  formatUnit
 }
